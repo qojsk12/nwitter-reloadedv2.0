d0dec63c944afb7368d892394d1a3537c8d09750
qojsk12 <qojsk12@naver.com>
2024-06-02 13:35:23 +0900

    수정

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path:
+      }
+    ]
+  }
+])
+
+function App() {
+  return <></>;
+}
+
+export default App;
